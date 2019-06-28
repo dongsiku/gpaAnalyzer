eb14592264c72756e4ddd83ddad25dfc55b32140
Dongsik <dongsiku+git@gmail.com>
2019-06-27 22:35:02 -0400

    Remove class in src/main.py

--- src/main.py
+++ src/main.py
@@ -13,33 +13,29 @@ from os import path
 from pathlib import Path
 import datetime
 from extract_data_from_html import extract_data_from_html
-
 strptime = datetime.datetime.strptime
 
 
-class AnalyzeGPA:
-    def __init__(self):
-        pass
+def main(self):
+    filename = self.get_filename()
+    if filename is False:
+        return 0
+    updated_date, gpa_list = extract_data_from_html(filename)
+    return gpa_list
 
-    def main(self):
-        filename = self.get_filename()
-        if filename is False:
-            return 0
-        updated_date, gpa_list = extract_data_from_html(filename)
-        return gpa_list
 
-    def get_filename(self):
-        root = tkinter.Tk()
-        root.withdraw()
-        filename = askopenfilename(filetypes=[("単位修得状況確認表.html", "*.html")],
-                                   initialdir=path.join(str(Path.home()), "Downloads"))
+def get_filename(self):
+    root = tkinter.Tk()
+    root.withdraw()
+    filename = askopenfilename(filetypes=[("単位修得状況確認表.html", "*.html")],
+                                initialdir=path.join(str(Path.home()), "Downloads"))
 
-        if filename == "":
-            print("no file")
-            return False
+    if filename == "":
+        print("no file")
+        return False
 
-        print(filename)
-        return filename
+    print(filename)
+    return filename
 
 
 if __name__ == "__main__":
