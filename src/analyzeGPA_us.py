7a37bcdb5832dd0fd3bd5a8685a6fd87d91ac9d1
Dongsik <dongsiku+git@gmail.com>
2019-06-27 23:04:29 -0400

    Rewrite src/analyzeGPA_us.py

--- src/analyzeGPA_us.py
+++ src/analyzeGPA_us.py
@@ -1,29 +1,47 @@
-from analyzeGPA import AnalyzeGPA
-import re
-
-
 class AnalyzeGPA_US:
-    def __init__(self):
-        pass
+    def __init__(self, gpa_list):
+        self.gpa_list = gpa_list
 
-    def main(self):
-        a_gpa = AnalyzeGPA()
+    def get_gpa(self):
+        sum_gp_by_credits = 0.0
+        sum_credits = 0.0
 
-        gdf, filename = a_gpa.extract_data_from_html()
-        print(self.calculateGPA(gdf))
+        for gpa_dict in self.gpa_list:
+            sum_gp_by_credits \
+                = self.grade_points_to_gp(gpa_dict["grade_points"]) * gpa_dict["credits"]
+            sum_credits = gpa_dict["credits"]
 
-    def calculateGPA(self, gdf):
-        point = 0.0
-        credit = 0.0
+        gpa_us = sum_gp_by_credits / sum_credits
+        return gpa_us
 
-        for i in range(len(gdf["授業科目"])):
-            if gdf["評点"][i] is not False:
-                point += gdf["評点"][i] * gdf["単位"][i]
-                credit += gdf["単位"][i]
-
-        return point/credit
+    def grade_points_to_gp(self, grade_points):
+        if grade_points > 92.4:  # A
+            return 4
+        elif grade_points > 89.9:  # A-
+            return 3.7
+        elif grade_points > 87.4:  # B+
+            return 3.33
+        elif grade_points > 82.4:  # B
+            return 3
+        elif grade_points > 79.9:  # B-
+            return 2.7
+        elif grade_points > 77.4:  # C+
+            return 2.3
+        elif grade_points > 72.4:  # C
+            return 2
+        elif grade_points > 69.9:  # C-
+            return 1.7
+        elif grade_points > 67.4:  # D+
+            return 1.3
+        elif grade_points > 62.4:  # D
+            return 1
+        elif grade_points > 59.0:  # D-
+            return 0.7
+        else:  # F
+            return 0
 
 
 if __name__ == "__main__":
-    an = AnalyzeGPA_US()
-    an.main()
+    from main import main
+    agpa_us = AnalyzeGPA_US(main())
+    print(agpa_us.get_gpa())
