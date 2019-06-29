2cbe521127c9a0e2c9c88d25b5352551de8bca49
Dongsik <dongsiku+git@gmail.com>
2019-06-28 20:42:49 -0400

    Fix bug: var name, gpa_dict

--- src/analyzeGPA_okadai.py
+++ src/analyzeGPA_okadai.py
@@ -2,22 +2,24 @@ import re
 
 
 class AnalyzeGPA_Okadai:
-    def __init__(self, gpa_list):
-        self.gpa_list = gpa_list
+    def __init__(self, cource_list):
+        self.cource_list = cource_list
 
     def get_gpa(self):
         sum_gp_by_credits = 0.0
         sum_credits = 0.0
 
         year_completed_set = set()
-        for gpa_dict in self.gpa_list:
-            year_completed_set.add(gpa_dict["year_completed"])
+        for cource_dict in self.cource_list:
+            year_completed_set.add(cource_dict["year_completed"])
 
         year_completed_list = []
         for yc in year_completed_set:
-            if not re.match(r".*またがり", yc):
+            if re.match(r"\d\d\d\d年度 第\d学期", yc):
                 year_completed_list.append(yc)
-        year_completed_list.append("その他")
+            elif not re.match(r".*またがり", yc):
+                if "その他" not in year_completed_list:
+                    year_completed_list.append("その他")
         year_completed_list.sort()
 
         tmp_yc_dict = {yc: 0.0 for yc in year_completed_list}
@@ -25,15 +27,15 @@ class AnalyzeGPA_Okadai:
         sum_credits_dict = tmp_yc_dict.copy()
         gpa_dict = tmp_yc_dict.copy()
 
-        for gpa_dict in self.gpa_list:
-            year_completed = gpa_dict["year_completed"]
+        for cource_dict in self.cource_list:
+            year_completed = cource_dict["year_completed"]
             if re.match(r".*またがり", year_completed):
                 tmp_yc = year_completed.rstrip("またがり").split(".", 1)
                 year_completed =\
                     "{} 第{}".format(tmp_yc[0].split(" ")[0], tmp_yc[1])
             sum_gp_by_credits_dict[year_completed] +=\
-                gpa_dict["gp"] * gpa_dict["credits"]
-            sum_credits_dict[year_completed] += gpa_dict["credits"]
+                cource_dict["gp"] * cource_dict["credits"]
+            sum_credits_dict[year_completed] += cource_dict["credits"]
 
         for yc in year_completed_list:
             gpa = sum_gp_by_credits_dict[yc] / sum_credits_dict[yc]
