9053352df07522e997d33692e84535e1fca90640
Dongsik <dongsiku+git@gmail.com>
2019-08-29 20:40:36 -0400

    copy_excel_file -> make_excel_file

--- src/export_to_excelfile.py
+++ src/export_to_excelfile.py
@@ -2,7 +2,7 @@ import openpyxl as px
 from datetime import datetime
 from shutil import copyfile
 from os import path
-from sys
+import sys
 
 
 class ExportToExcelFile:
@@ -12,13 +12,14 @@ class ExportToExcelFile:
             "gpaAnalyzer_{}.xlsx".format(datetimeNow.strftime('%Y%m%d_%H%M%S'))
         self.excel_filename = path.join(dirname, self.excel_filename)
 
-    def copy_excel_file(self):
+    def make_excel_file(self):
         original_excel_filename =\
             path.join(path.dirname(path.abspath(sys.argv[0])),
                                    "gpaAnalyzer.xlsx")
         copyfile(original_excel_filename, self.excel_filename)
 
     def export_to_excel_file(self):
-        wb = px.load_workbook(filename)
+        self.make_excel_file()
+        wb = px.load_workbook(self.excel_filename)
         ws_gpa_okadai = wb['Okadai']
         ws_gpa_summary = wb['Summary']
